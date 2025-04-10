abfcd7e762e2ac219ed4fd61b3393f1dba5151d7
Nael Saleh <naels141@gmail.com>
2025-04-10 12:30:09 +0200

    Update styles and refactor header component

--- components/my-header.js
+++ components/my-header.js
@@ -1,44 +1,63 @@
 ﻿class MyHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.basePath = this.getBasePath();
+    }
+
     connectedCallback() {
-        const isInPages = location.pathname.includes('/pages/');
-        const base = isInPages ? '..' : '.';
+        this.render();
+        this.highlightActiveLink();
+    }
 
+    getBasePath() {
+        return location.pathname.includes('/pages/') ? '..' : '.';
+    }
+
+    render() {
         this.innerHTML = `
-      <header>
-        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="${base}/index.html">
-              <img src="${base}/assets/images/logo-24.png" alt="logo" width="30" height="24" />
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-              data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-              aria-label="التبديل بين العناصر">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="${base}/pages/engagement.html">الخطوبة</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="${base}/pages/home.html">تجهيزات المنزل</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="${base}/pages/marriage.html">الزواج</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-    `;
+          <header>
+            <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="${this.basePath}/index.html" aria-label="Main">
+                  <img src="${this.basePath}/assets/images/logo-24.png" alt="logo" width="30" height="24" loading="lazy" />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                  aria-label="Toggle Navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    ${this.getNavLinks()}
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </header>
+        `;
+    }
 
+    getNavLinks() {
+        const navLinks = [
+            { href: `${this.basePath}/pages/engagement.html`, text: 'الخطوبة', ariaLabel: 'Engagement' },
+            { href: `${this.basePath}/pages/home.html`, text: 'المنزل', ariaLabel: 'Home' },
+            { href: `${this.basePath}/pages/marriage.html`, text: 'الزواج', ariaLabel: 'Marriage' }
+        ];
+
+        return navLinks
+            .map(link => `<li class="nav-item"><a class="nav-link" href="${link.href}" aria-label="${link.ariaLabel}">${link.text}</a></li>`)
+            .join('');
+    }
+
+    highlightActiveLink() {
         const currentPage = location.pathname.split('/').pop();
 
         this.querySelectorAll('.nav-link').forEach(link => {
             const linkPage = link.getAttribute('href').split('/').pop();
             if (linkPage === currentPage) {
                 link.classList.add('active');
+                link.setAttribute('aria-current', 'page');
+                link.addEventListener('click', event => event.preventDefault());
             }
         });
     }
