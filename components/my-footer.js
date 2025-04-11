1c6d4eb617d735bbd2123921bdce6bb45bcd03f8
Nael Saleh <naels141@gmail.com>
2025-04-11 12:06:49 +0200

    Refactor MyFooter class for improved link handling

--- components/my-footer.js
+++ components/my-footer.js
@@ -1,23 +1,45 @@
 ﻿class MyFooter extends HTMLElement {
-    connectedCallback() {
-        const isInPages = location.pathname.includes('/pages/');
-        const base = isInPages ? '..' : '.';
+    constructor() {
+        super();
+        this.basePath = this.getBasePath();
+    }
 
+    connectedCallback() {
+        this.render();
+        this.updateLinkEvents();
+    }
+
+    getBasePath() {
+        return location.pathname.includes('/pages/') ? '..' : '.';
+    }
+
+    render() {
         this.innerHTML = `
           <footer class="py-3">
               <ul class="nav justify-content-center">
                 <li class="nav-item">
-                  <a href="${base}/pages/privacy.html" class="nav-link px-2">سياسة الخصوصية</a>
+                  <a href="${this.basePath}/pages/privacy.html" class="nav-link px-2">سياسة الخصوصية</a>
                 </li>
                 <li class="nav-item">
-                  <a href="${base}/pages/terms.html" class="nav-link px-2">شروط الاستخدام</a>
+                  <a href="${this.basePath}/pages/terms.html" class="nav-link px-2">شروط الاستخدام</a>
                 </li>
                 <li class="nav-item">
-                  <a href="${base}/pages/contact.html" class="nav-link px-2">اتصل بنا</a>
+                  <a href="${this.basePath}/pages/contact.html" class="nav-link px-2">اتصل بنا</a>
                 </li>
               </ul>
           </footer>
         `;
+    }
+
+    updateLinkEvents() {
+        const currentPage = location.pathname.split('/').pop();
+
+        this.querySelectorAll('.nav-link').forEach(link => {
+            const linkPage = link.getAttribute('href').split('/').pop();
+            if (linkPage === currentPage) {
+                link.addEventListener('click', event => event.preventDefault());
+            }
+        });
     }
 }
 
