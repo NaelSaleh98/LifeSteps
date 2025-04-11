ee377a363e0624b707ff8c494f21d20e30ddc2cd
Nael Saleh <naels141@gmail.com>
2025-04-11 11:47:07 +0200

    Enhance styles for lazy-loaded images and footer

--- components/my-footer.js
+++ components/my-footer.js
@@ -4,23 +4,20 @@
         const base = isInPages ? '..' : '.';
 
         this.innerHTML = `
-      <footer class="bg-primary text-white text-center py-3">
-        <div class="container">
-          <p>&copy; 2025 رحلة الزواج. جميع الحقوق محفوظة.</p>
-          <ul class="list-inline">
-            <li class="list-inline-item">
-              <a href="${base}/pages/privacy.html" class="text-white">سياسة الخصوصية</a>
-            </li>
-            <li class="list-inline-item">
-              <a href="${base}/pages/terms.html" class="text-white">شروط الاستخدام</a>
-            </li>
-            <li class="list-inline-item">
-              <a href="${base}/pages/contact.html" class="text-white">اتصل بنا</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    `;
+          <footer class="py-3">
+              <ul class="nav justify-content-center">
+                <li class="nav-item">
+                  <a href="${base}/pages/privacy.html" class="nav-link px-2">سياسة الخصوصية</a>
+                </li>
+                <li class="nav-item">
+                  <a href="${base}/pages/terms.html" class="nav-link px-2">شروط الاستخدام</a>
+                </li>
+                <li class="nav-item">
+                  <a href="${base}/pages/contact.html" class="nav-link px-2">اتصل بنا</a>
+                </li>
+              </ul>
+          </footer>
+        `;
     }
 }
 
